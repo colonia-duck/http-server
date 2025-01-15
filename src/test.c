ca03c83c2759d4304b262dffc1055f96f55e679d
Thomas Clarke <colonia_duck@posteo.net>
2025-01-15 17:56:26 +0000

    updated directorys

--- src/test.c
+++ src/test.c
@@ -1,7 +1,6 @@
-#include "src/server.h"
+#include "server.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 void launch(struct Server *server) {
@@ -9,7 +8,6 @@ void launch(struct Server *server) {
   int address_length;
   char buffer[10000];
   while (1) {
-
     printf("Listening on 127.0.0.1\n===== WAITING FOR CONNECTION =====\n");
     address_length = sizeof(server->address);
     new_socket = accept(server->socket, (struct sockaddr *)&server->address,
@@ -17,15 +15,13 @@ void launch(struct Server *server) {
     read(new_socket, buffer, 10000);
     printf("%s\n", buffer);
     printf("===== CONNECTION RECIVED =====\n");
-
-    const char *hello =
+    const char hello[] =
         "HTTP/1.1 200 OK\nDate: Sat, 11 Jan 2025 21:05:22 "
         "GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Sat, "
         "11 Jan 2025 21:05:22 GMT\nSat, 11 Jan 2025 21:05:22 "
         "GMT\nContent-Type: text/html\nConnection: "
         "Closed\n\n<html><body><h1>Hello world</h1></body></html>";
-
-    write(new_socket, hello, strlen(hello));
+    write(new_socket, hello, sizeof(hello) - 1);
     close(new_socket);
   }
 }
